e58c2016949ad57edbee85783b3329764b386744
caiqichang <cai_qichang@qq.com>
2024-02-05 11:12:43 +0800

    .

--- toolkit/sudo.bat
+++ toolkit/sudo.bat
@@ -1,3 +1,4 @@
 @echo off
-powershell -Command "(($arg='/k cd /d '+$pwd+' && %*') -and (Start-Process cmd -Verb RunAs -ArgumentList $arg))| Out-Null"
+powershell -Command ^
+"(($arg='/k cd /d '+$pwd+' && %*') -and (Start-Process cmd -Verb RunAs -ArgumentList $arg))| Out-Null"
 @echo on
